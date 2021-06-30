ebe6e414a17a6e1948da6c71a8692ec631999e8e
David Nemeth Csoka <encse@csokavar.hu>
2021-06-30 07:04:34 +0200

    fonts

--- src/index.ts
+++ src/index.ts
@@ -163,15 +163,18 @@ function pageFromMarkdown(isFrontPage:boolean, i18n: I18n, markdown: string): st
         |     <meta charset="UTF-8">
         |     <meta name="viewport" content="width=device-width, initial-scale=1.0">
         |     <title>${titleText} - ${metadata.title}</title>
+        |     <link rel="preconnect" href="https://fonts.googleapis.com">
+        |     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        |     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Francois+One&family=Merriweather:ital,wght@0,400;0,700;1,400;1,700&display=swap">
         |     <link rel="stylesheet" href="/css/site.css?v=${version('site/css/site.css')}">
-        |    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-203054-6"></script>
-        |    <script async src="/index.js?v=${version('site/index.js')}" type="module"></script>
-        |    <script>
-        |         window.dataLayer = window.dataLayer || [];
-        |         function gtag(){dataLayer.push(arguments);}
-        |         gtag('js', new Date());
-        |         gtag('config', 'UA-203054-6');
-        |     </script>
+        |     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-203054-6"></script>
+        |     <script async src="/index.js?v=${version('site/index.js')}" type="module"></script>
+        |     <script>
+        |          window.dataLayer = window.dataLayer || [];
+        |          function gtag(){dataLayer.push(arguments);}
+        |          gtag('js', new Date());
+        |          gtag('config', 'UA-203054-6');
+        |      </script>
         | </head>
         | <body ${bodyClass}>
         | <header>
